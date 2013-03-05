301891e1187d8d06fa610bf7a7a2ea6248077b61
NeilRatna <neil.ratna@gmail.com>
2013-03-04 19:18:34 -0500

    30 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,59 @@
 # Exercises for chapter 2: Problems 2.1, 2.2, 2.3, and 2.4 in Think Python
 
-# Name
+# Neil Ratna
+
+#2.1
+# python thinks you want to convert from an octal number to a decimal number. Zipcode 02492 is invalid because 9 is not a valid octal number. 
+
+#2.2
+
+5
+x = 5
+x + 1
+print x
+output is 5
+
+print 5
+print x = 5
+print X + 1
+
+# this will show exactly what i just typed out
+
+#2.3
+
+width = 17
+height = 12.0
+delimiter = '.'
+
+print width/2 #displays 8
+print width/2.0 #displays 8.5
+print height/3 #displays 4.0
+print 1 + 2 * 5 #displays 11
+print delimiter * 5 #displays .....
+
+#2.4 #1
+
+pi = 3.14159
+r = 5.0
+volume = 4.0/3.0 * pi * r ** 3
+print volume #displays 523.59833333
+
+#2.4 #2
+
+wholesale = 24.95 * 0.6 * 60 + 0.75 * (60-1) +3
+print wholesale #displays 945.45
+
+#2.4 #3
+
+start = (6*60+52) * 60
+easy = (8*60+15) * 2
+tempo = (7*60+12) *3
+finish_hour = (start + easy +tempo)/(60*60.0)
+finish_floor = (start + easy + tempo)/(60*60)
+finish_min = (finish_hour - finish_floor)*60
+print 'Home for breakfast at %d:%d' % (finish_hour,finish_min)
+#displays 7:30
+
+
+
+
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,40 @@
 # Exercises for chapter 3: Problems 3.1, 3.2, 3.3, and 3.4 in Think Python
 
-# Name
+# Neil Ratna
+
+#3.1 & 3.2
+import math
+
+#def repeat_lyrics():
+#	print_lyrics()
+#	print_lyrics()
+
+#def print_lyrics():
+#	print "I'm a lumberjack, and I'm okay."
+#	print "I sleep all night and I work all day"
+
+#repeat_lyrics()
+
+#3.1 get a NameError that repeat_lyrics is not defined
+#3.2 this program runs ok
+
+#3.3
+
+#def right_justify(s):
+#	print (''*(70-len(s))+s)
+
+#right_justify('allen')
+
+#3.4
+
+def do_twice(f):
+	f()
+	f()
+
+def print_spam():
+	print 'spam'
+
+do_twice(print_spam)
+
+#1. displays spam twice
+
